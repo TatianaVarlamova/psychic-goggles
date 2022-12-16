8460a44a88d3af6597a42fddac7193965c9da1d0
Tatiana <zippy.2017@mail.ru>
2022-12-16 23:44:18 +0300

    Задание4

---
+++ DZ4/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число ");
+int a = int.Parse(Console.ReadLine()!);
+if (a % 2 == 0)
+{
+Console.WriteLine("число четное");
+}
+else
+{
+Console.WriteLine("число нечетное");
+}
