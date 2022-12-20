3b5f1efc2f25d8847090aab8e4f49d2af4387b33
Tatiana <zippy.2017@mail.ru>
2022-12-20 18:21:10 +0300

    Задание13

---
+++ DZ13/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите любое число ");
+int a = int.Parse(Console.ReadLine()!);
+char[] arr = a.ToString().ToCharArray();
+int n = arr.Length;
+if ( n > 2 )
+{
+    Console.WriteLine(arr[2]);
+}
+else
+{
+    Console.WriteLine("нет третьего числа");
+}
