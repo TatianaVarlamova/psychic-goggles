d0f16c919cda917826423d6416ee01a1a31702e4
Tatiana <zippy.2017@mail.ru>
2022-12-21 13:56:54 +0300

    задание19

---
+++ DZ19/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите пятизначное число");
+string num = Console.ReadLine();
+if (num[1] == num[3] && num[0] == num[4]) 
+{
+    Console.Write("да,число является палиндромом");
+}
+else
+{
+    Console.Write("нет,это число не является палиндромом");
+}
