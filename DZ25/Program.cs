93a3c9e02c7e7e9c81f72aaf8aeed43357aa7ade
Tatiana <zippy.2017@mail.ru>
2022-12-22 19:00:27 +0300

    задание25

---
+++ DZ25/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("ВВедите число ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("ВВедите степень числа ");
+int b = int.Parse(Console.ReadLine()!);
+int stepen = a;
+for (int i = 1; i < b; i++)
+{
+    stepen = stepen * a;
+}
+Console.WriteLine(stepen);
