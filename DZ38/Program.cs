6868c57465934c8dc49ed2cbd9ab6e8dce1041f4
Tatiana <zippy.2017@mail.ru>
2022-12-25 13:08:48 +0300

    задание38

---
+++ DZ38/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine ("Введите размер массива");
+int numb = Convert.ToInt32(Console.ReadLine());
+double[] Array =new double[numb];
+for (int i=0; i<Array.Length; i++)
+{
+    Array[i] = new Random().Next(1,30);
+    Console.Write(Array[i]+ ", ");
+}
+double min = Array[0];
+double max = Array[0];
+for(int i = 0; i < Array.Length; i++)
+{
+    if (Array[i] > max)
+        {
+            max = Array[i];
+        }
+    if (Array[i] < min)
+        {
+            min = Array[i];
+        }
+}
+Console.WriteLine($"Разница между максимальным и минимальным значением = {max - min}");
