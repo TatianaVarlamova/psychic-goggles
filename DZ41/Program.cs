9a2d355122925b4e6c6e8a01a2b81c2dd135739f
Tatiana <zippy.2017@mail.ru>
2022-12-26 22:35:50 +0300

    задание41/

--- DZ41/Program.cs
+++ DZ41/Program.cs
@@ -1,4 +1,10 @@
-﻿Console.WriteLine("Введите числа через пробел: ");
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// Пример:
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Введите числа через пробел: ");
 string inputText = Console.ReadLine(); 
 string[] stringArray = inputText.Split(' ');
 int[] arr = new int[stringArray.Length];
