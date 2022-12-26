ed1711731c347c5abe80ac76d8f05ea975f51b74
Tatiana <zippy.2017@mail.ru>
2022-12-26 21:59:00 +0300

    задание41

---
+++ DZ41/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите числа через пробел: ");
+string inputText = Console.ReadLine(); 
+string[] stringArray = inputText.Split(' ');
+int[] arr = new int[stringArray.Length];
+
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = int.Parse(stringArray[i]);
+}
+
+for (int i = 0; i < arr.Length; i++)
+{
+    Console.Write(arr[i] + " ");
+}
+int sum = 0;
+for (int i = 0; i < arr.Length; i++)
+if (arr[i] > 0)
+    sum++;
+
+Console.WriteLine($" {sum}  больше ноля");
+
