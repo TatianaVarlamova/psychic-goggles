4b1680e048a84745d6b643145cd3dcd6d3752808
Tatiana <zippy.2017@mail.ru>
2022-12-28 21:39:06 +0300

    задание47

---
+++ DZ47/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите количество строк: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите количество столбцов: ");
+int n = int.Parse(Console.ReadLine()!);
+double[,] arr = new double[m,n];
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+     for (int j = 0; j < arr.GetLength(1); j++)
+     {
+         double number = new Random().Next(-10, 11) + new Random().NextDouble();
+         arr[i, j] = Math.Round(number, 2);
+     }
+ }
+for (int i = 0; i < arr.GetLength(0); i++)
+ {
+     for (int j = 0; j < arr.GetLength(1); j++)
+     {
+         Console.Write(arr[i, j] + " ");
+     }
+     Console.WriteLine();
+ }    
