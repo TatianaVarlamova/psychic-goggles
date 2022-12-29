d3623af845a6ae3c9a46401436eb21a047fb93f4
Tatiana <zippy.2017@mail.ru>
2022-12-29 22:11:55 +0300

    задание52

---
+++ DZ52/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("Введите количество строк: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите количество столбцов: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine();
+int[,] arr = new int[m,n];
+for (int i = 0; i < arr.GetLength(0); i++)
+ {
+     for (int j = 0; j < arr.GetLength(1); j++)
+     {
+         arr[i, j] = new Random().Next(0, 10);
+     }
+ }
+for (int i = 0; i < arr.GetLength(0); i++)
+ {
+     for (int j = 0; j < arr.GetLength(1); j++)
+     {
+         Console.Write(arr[i, j] + " ");
+     }
+     Console.WriteLine();
+ }
+Console.WriteLine();
+ for (int j = 0; j < arr.GetLength(1); j++)
+{
+    double sum = 0;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        sum = (sum + arr[i, j]);
+    }
+    sum = sum / m;
+    Console.Write(sum + "; ");
+}
+
