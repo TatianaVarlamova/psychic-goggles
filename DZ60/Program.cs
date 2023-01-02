8ce7d928b9c4c29d04744438da552acfb1e3ffda
Tatiana <zippy.2017@mail.ru>
2023-01-02 21:56:39 +0300

    задание60

---
+++ DZ60/Program.cs
@@ -0,0 +1,27 @@
+﻿int [,,] array3d = new int[2,2,2];
+
+for (int i = 0; i < array3d.GetLength(0); i++)
+{
+    for (int j = 0; j < array3d.GetLength(1); j++)
+    {
+        for (int k = 0; k < array3d.GetLength(2); k++)
+        {
+              array3d[i, j, k] = new Random().Next(10, 100);
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+for (int i = 0; i < array3d.GetLength(0); i++)
+{
+    for (int j = 0; j < array3d.GetLength(1); j++)
+    {
+        for (int k = 0; k < array3d.GetLength(2); k++)
+        {
+             Console.Write($"{array3d[i, j, k]}{(i, j, k)}  ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
