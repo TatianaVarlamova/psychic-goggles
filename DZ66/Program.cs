d5b609c5a8a299dbf13fbc3f714aeb9f810fd455
Tatiana <zippy.2017@mail.ru>
2023-01-03 17:51:50 +0300

    задание66

---
+++ DZ66/Program.cs
@@ -0,0 +1,13 @@
+﻿int PrintSum(int M, int N)
+{
+    if(M == N) return N;
+    return (M + PrintSum(M+1, N));
+}
+
+Console.Clear();
+Console.WriteLine("Введите начальное значение М: ");
+int M = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите конечное значение N: ");
+int N = int.Parse(Console.ReadLine()!);
+Console.WriteLine();
+Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N: {PrintSum(M, N)}");
