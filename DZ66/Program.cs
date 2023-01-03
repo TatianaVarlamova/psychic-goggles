b652e1d3b9f000ba453952b71e50cf2cb6340282
Tatiana <zippy.2017@mail.ru>
2023-01-03 17:54:41 +0300

    задание66'

--- DZ66/Program.cs
+++ DZ66/Program.cs
@@ -1,4 +1,9 @@
-﻿int PrintSum(int M, int N)
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт 
+// сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int PrintSum(int M, int N)
 {
     if(M == N) return N;
     return (M + PrintSum(M+1, N));
