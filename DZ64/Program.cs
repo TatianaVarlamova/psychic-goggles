42cfb3f0ce31d303613af0f2bcf06e00d7e6fe31
Tatiana <zippy.2017@mail.ru>
2023-01-03 18:10:16 +0300

    задание64

---
+++ DZ64/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введите N: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine(PrintNumbers(n));
+
+string PrintNumbers(int number)
+{ 
+    if (number == 1) return "1";
+    return( number + " " + PrintNumbers(number - 1));
+}
